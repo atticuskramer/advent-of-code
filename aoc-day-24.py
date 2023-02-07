24cc5f3724d1c31fe32398df9d99493ba07bab4d
atticuskramer <50434649+atticuskramer@users.noreply.github.com>
2023-02-07 14:43:55 -0600

    Day 24 Part 1: Almost finished with a_star method
    Now getting the correct answer for the example, but the full input still gives the wrong answer.

--- aoc-day-24.py
+++ aoc-day-24.py
@@ -33,48 +33,45 @@ test_input = """#.######
 #<^v^^>#
 ######.#"""
 
+from queue import deque
+
 class Blizzard:
-    def __init__(self, row, col, dir_str):
+    def __init__(self, row, col, dir_str, drow=None, dcol=None):
         self.row = row
         self.col = col
         self.dir_str = dir_str
-        if dir_str == '>':
-            self.drow = 0
-            self.dcol = 1
-        elif dir_str == '<':
-            self.drow = 0
-            self.dcol = -1
-        elif dir_str == '^':
-            self.drow = -1
-            self.dcol = 0
-        elif dir_str == 'v':
-            self.drow = 1
-            self.dcol = 0
+        if drow is None or dcol is None:
+            if dir_str == '>':
+                self.drow = 0
+                self.dcol = 1
+            elif dir_str == '<':
+                self.drow = 0
+                self.dcol = -1
+            elif dir_str == '^':
+                self.drow = -1
+                self.dcol = 0
+            elif dir_str == 'v':
+                self.drow = 1
+                self.dcol = 0
+            else:
+                raise Exception(f'Bad direction string for wind: {dir_str}')
         else:
-            raise Exception(f'Bad direction string for wind: {dir_str}')
+            self.drow = drow
+            self.dcol = dcol
     
     def __str__(self):
         return self.dir_str
-        
-    def move(self,right, bottom, left, top, reverse=False):
-        if reverse:
-            self.row -= self.drow
-            self.col -= self.dcol
-        else:
-            self.row += self.drow
-            self.col += self.dcol
-        if self.row <= top:
-            self.row = bottom - 1
-        elif self.row >= bottom:
-            self.row = top + 1
-        elif self.col <= left:
-            self.col = right - 1
-        elif self.col >= right:
-            self.col = left + 1
-    
-    # TODO: Figure out if these should just be tuples the entire time?        
-    def get_tuple(self):
-        return (self.row, self.col, self.dir_str)
+            
+    def moved(self, moves, right, bottom, left, top):
+        new_row = self.row + self.drow * moves
+        new_col = self.col + self.dcol * moves
+        new_row = top + (new_row % (bottom - top - 1))
+        new_col = left + (new_col % (right - left - 1))
+        if new_row == top:
+            new_row = bottom - 1
+        if new_row == left:
+            new_row = right - 1
+        return Blizzard(new_row, new_col, self.dir_str, self.drow, self.dcol)
             
 class Valley:
     
@@ -84,124 +81,121 @@ class Valley:
         self.left = 0
         self.bottom = len(lines) - 1
         self.right = len(lines[0]) - 1
-        self.grid = [[[] for _ in range(self.right+1)] for _ in range(self.bottom+1)]
+        self.start = (self.top, self.left + 1)
+        self.goal = (self.bottom, self.right - 1)
         self.blizzards = []
         for row, line in enumerate(lines):
             for col, char in enumerate(line):
                 if char in ['>', 'v', '<', '^']:
                     bliz = Blizzard(row, col, char)
-                    self.grid[row][col].append(bliz)
                     self.blizzards.append(bliz)
-        self.explorer = (0,1)
-        self.states = dict()
+        self.explorer = self.start
+        # This is a janky way to bootstrap this into existence
+        self.blizzard_positions = []
+        self.blizzard_positions.append(self.blizzards_after(0))
         
-    def __str__(self):
-        result_str = ''
-        for irow, row in enumerate(self.grid):
-            for icol, bliz_list in enumerate(row):
-                if irow == self.explorer[0] and icol == self.explorer[1]:
+    # def __str__(self):
+    #     result_str = ''
+    #     for irow, row in enumerate(self.grid):
+    #         for icol, bliz_list in enumerate(row):
+    #             if irow == self.explorer[0] and icol == self.explorer[1]:
+    #                 result_str += 'E'
+    #             elif (irow == self.top and icol == self.left + 1) or (irow == self.bottom and icol == self.right - 1):
+    #                 result_str += '.'
+    #             elif irow == self.top or irow == self.bottom or icol == self.left or icol == self.right:
+    #                 result_str += '#'
+    #             elif len(bliz_list) > 1:
+    #                 result_str += str(len(bliz_list))
+    #             elif len(bliz_list) == 1:
+    #                 result_str += str(bliz_list[0])
+    #             else:
+    #                 result_str += '.'
+    #         result_str += '\n'
+    #     return result_str
+    
+    def print_state(self, state):
+        new_blizzards = self.blizzards_after(state[0])
+        explorer = state[1:]
+        result_str = f'Path length: {state[0]}\n'
+        for row in range(self.bottom + 1):
+            for col in range(self.right + 1):
+                if (row, col) == explorer:
                     result_str += 'E'
-                elif (irow == self.top and icol == self.left + 1) or (irow == self.bottom and icol == self.right - 1):
+                elif (row, col) == self.start or (row, col) == self.goal:
                     result_str += '.'
-                elif irow == self.top or irow == self.bottom or icol == self.left or icol == self.right:
+                elif row == self.bottom or row == self.top or col == self.left or col == self.right:
                     result_str += '#'
-                elif len(bliz_list) > 1:
-                    result_str += str(len(bliz_list))
-                elif len(bliz_list) == 1:
-                    result_str += str(bliz_list[0])
+                elif (row, col) in new_blizzards and len(new_blizzards[(row,col)]) == 1:
+                    result_str += new_blizzards[(row, col)]
+                elif (row, col) in new_blizzards:
+                    result_str += str(len(new_blizzards[(row, col)]))
                 else:
                     result_str += '.'
             result_str += '\n'
-        return result_str
-        
-    def get_state(self):
-        return tuple([bliz.get_tuple() for bliz in self.blizzards] + [self.explorer])
-        
-    def move_blizzards(self, reverse=False):
-        for bliz in self.blizzards:
-            self.grid[bliz.row][bliz.col].remove(bliz)
-            bliz.move(self.right, self.bottom, self.left, self.top, reverse=reverse)
-            self.grid[bliz.row][bliz.col].append(bliz)
-            
-    def explore(self, path_length=0, best_above=999999, verbose=False):
-        if verbose:
-            print(self)
-            input()
-        # -1. Check if we have reached the end
-        if self.explorer == (self.bottom-1, self.right-1):
-            return path_length + 1
-        # 0. Check if we have seen this state before at this time or less. If we have,
-        #    ignore this, if we haven't, add this state to the explored
-        state = self.get_state()
-        if state in self.states and self.states[state] <= path_length:
-            return 999999
+        print(result_str)
+    
+    def blizzards_after(self, time):
+        if len(self.blizzard_positions) > time:
+            return self.blizzard_positions[time]
         else:
-            self.states[state] = path_length
-        # 0a. Also check if the current distance from the finish is longer than the
-        #     shortest path found so far. If it is, we can ignore this branch
-        distance_to_finish = abs(self.bottom - self.explorer[0]) + abs((self.right - 1) - self.explorer[1])
-        if distance_to_finish >= best_above:
-            return 999999
-        # TODO
-        # 1. Move the blizzards
-        self.move_blizzards()
-        # 2. Get the valid spots that we can move to
-        to_explore = []
+            assert len(self.blizzard_positions) == time
+        bliz_dict = {}
+        for bliz in self.blizzards:
+            moved = bliz.moved(time, self.right, self.bottom, self.left, self.top)
+            location = (moved.row, moved.col)
+            if location in bliz_dict:
+                bliz_dict[location] += moved.dir_str
+            else:
+                bliz_dict[location] = bliz.dir_str
+        self.blizzard_positions.append(bliz_dict)
+        return bliz_dict
+        
+    def is_goal(self, state):
+        return state[1:] == self.goal
+    
+    def get_next_states(self, state):
+        next_path_length = state[0] + 1
+        new_blizzards = self.blizzards_after(next_path_length)
+        next_states = []
         for (drow, dcol) in [(1,0), (0,1), (0,0), (-1,0), (0,-1)]:
-            new_row = self.explorer[0] + drow
-            new_col = self.explorer[1] + dcol
-            if (new_row, new_col) == (0,1) or (self.top < new_row < self.bottom and 
+            new_row = state[1] + drow
+            new_col = state[2] + dcol
+            if ((new_row, new_col) == self.start or 
+                (new_row, new_col) == self.goal or 
+                (self.top < new_row < self.bottom and 
                 self.left < new_col < self.right and 
-                len(self.grid[new_row][new_col]) == 0):
-                to_explore.append((new_row, new_col))
-        # 3. Sort the spots by how close they are to the exit
-        #   - Because the exit is always in the bottom right, we can actually skip this.
-        #     the options will always be sorted already since we start with the two that
-        #     get closer and end with the two that get further away.
-        # 4. Explore the now-sorted options
-        cur_explorer = self.explorer
-        best = 999999
-        for next_explorer in to_explore:
-            self.explorer = next_explorer
-            pl = self.explore(path_length + 1, min(best_above, best), verbose=verbose)
-            best = min(pl, best)
-        # 5. Reverse the blizzards and reset the explorer
-        self.move_blizzards(reverse=True)
-        self.explorer = cur_explorer
-        return best
-        
-    # Stubs for A_star use conversion
-    def is_goal(self):
-        pass
+                (new_row, new_col) not in new_blizzards)):
+                next_states.append((next_path_length, new_row, new_col))
+        return next_states
     
-    def get_next_states(self):
-        pass
-    
-    def get_f_score(self):
-        pass
+    def get_f_score(self, state):
+        return state[0] + abs(state[1] - self.goal[0]) + abs(state[2] - self.goal[1])
         
-def a_star_explore(valley, is_goal_func):
+def a_star_explore(valley):
     # 0. Keep track of a set of explored states, and a queue of next states, both starting
     #    with just the initial state.
-    queue = [valley]
+    # Our state will just be a triple of (path_length, explorer_row, explorer_col)
+    start_state = (0, valley.start[0], valley.start[1])
+    queue = deque([start_state])
     explored = set(queue)
     # While there are states to check left in the queue...
     while(queue):
         # 1. Pop the first item in the queue and set that to be our current state
-        cur_state = queue.pop(0)
+        cur_state = queue.popleft()
+        valley.print_state(cur_state)
         # 2. Check if the current state is the goal state, if it is, return the path(_length)
         #    from the start state to the goal
-        if cur_state.is_goal():
-            return cur_state.path_length
+        if valley.is_goal(cur_state):
+            return cur_state[0]
         # 3. Get all of the next possible states from the current state that are not already
         #    in our set of explored states
-        next_states = [state for state in cur_state.get_next_states() if state not in explored]
+        next_states = [state for state in valley.get_next_states(cur_state) if state not in explored]
         # 4. Insert those states into the queue, keeping the queue sorted based on increasing
         #    f score, where f(state) = length from start to state + manhattan distance to goal state
         for state in next_states:
             inserted = False
             for i, q_state in enumerate(queue):
-                if state.get_f_score() < q_state.get_f_score():
+                if valley.get_f_score(state) < valley.get_f_score(q_state):
                     queue.insert(i, state)
                     inserted = True
                     break
@@ -213,7 +207,10 @@ def a_star_explore(valley, is_goal_func):
         
 def part_1(input_str, verbose=False):
     valley = Valley(input_str)
-    return valley.explore(verbose=verbose)
+    # for i in range(10):
+    #     print(valley.blizzards_after(i))
+    return a_star_explore(valley)
         
 print(part_1(test_input))
-print(part_1(full_input))
+
+# print(part_1(full_input))
