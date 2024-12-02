0dff8887c55a7aa28f9c33b21cd32fe141510ba9
atticuskramer <50434649+atticuskramer@users.noreply.github.com>
2024-12-02 14:21:25 -0600

    2024 Day 1 Part 2 Completed

--- 2024/day_1.py
+++ 2024/day_1.py
@@ -21,13 +21,24 @@ def part_1(input_str):
     return sum(abs(a - b) for a,b in zip(sorted(list_1), sorted(list_2)))
 
 def part_2(input_str):
-    pass
+    list_1, list_2 = num_lists_from_input(input_str)
+    list_2_dict = dict()
+    similarity_score = 0
+    for num in list_2:
+        if num in list_2_dict:
+            list_2_dict[num] += 1
+        else:
+            list_2_dict[num] = 1
+    for num in list_1:
+        similarity_score += num * list_2_dict.get(num, 0)
+    return similarity_score
 
 def main():
     with open('aoc_day_1_input.txt') as input_file:
         full_input = input_file.read()
     print(part_1(test_input))
     print(part_1(full_input))
+    print(part_2(test_input))
     print(part_2(full_input))
 
 if __name__ == '__main__':
