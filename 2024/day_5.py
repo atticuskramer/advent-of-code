a1192e0010666d1c4671e0aaf9746ce102cd59bf
atticuskramer <50434649+atticuskramer@users.noreply.github.com>
2024-12-07 22:41:14 +0000

    Completed 2024 day 5 part 2

--- 2024/day_5.py
+++ 2024/day_5.py
@@ -51,7 +51,37 @@ def part_1(input_str):
     return correct_middles
             
 def part_2(input_str):
-    pass
+    rules_str, updates_str = input_str.split('\n\n')
+    rules = [[int(num) for num in rule.split('|')] for rule in rules_str.split('\n')]
+    updates = [[int(num_str) for num_str in update.split(',')] for update in updates_str.split('\n')]
+    corrected_middles = 0
+    # Create a dictionary with the 'before' numbers as keys, and sets of all
+    # corresponding 'after' numbers as values 
+    rule_dict = dict()
+    for before, after in rules:
+        if before in rule_dict:
+            rule_dict[before].add(after)
+        else:
+            rule_dict[before] = set([after])
+    # For each update, create a new list and copy over the items, keeping them
+    # sorted as we go.
+    for update in updates:
+        corrected = []
+        for page in update:
+            i = 0
+            while i < len(corrected) and page in rule_dict.get(corrected[i], set()):
+                i += 1
+            corrected.insert(i, page)
+        # If the corrected list is the same as the original list, don't include
+        # it in our sum.
+        # We could improve efficiency by abstracting out the check for a sorted
+        # list from part 1 and checking if sorted before resorting, but this is
+        # asymptotically the same, so meh
+        if corrected != update:
+            corrected_middles += corrected[len(corrected)//2]
+    return corrected_middles
+            
+    
 
 def main():
     with open('aoc_day_5_input.txt') as input_file:
