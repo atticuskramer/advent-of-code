b8de3889bb20ef10536dbdec67f09ccb5bd9de80
atticuskramer <50434649+atticuskramer@users.noreply.github.com>
2024-12-13 00:59:15 +0000

    Completed 2024 Day 6 Part 2

--- 2024/day_6.py
+++ 2024/day_6.py
@@ -17,7 +17,7 @@ class Elf_Grid():
         self.height = len(lines)
         self.width = len(lines[0])
         self.empty = set()
-        self.visited = set()
+        self.visited = dict()
         self.obstacles = set()
         for row, line in enumerate(lines):
             for col, char in enumerate(line):
@@ -28,8 +28,8 @@ class Elf_Grid():
                 elif char == '^':
                     # For now, we assume that the guard always will start facing north
                     self.guard = (row,col)
-                    self.visited.add((row,col))
                     self.direction = (-1, 0)
+                    self.visited[(row,col)] = [self.direction]
 
     def turn_right(self):
         next_directions = {
@@ -45,16 +45,44 @@ class Elf_Grid():
     
     def is_valid(self, position):
         return 0 <= position[0] < self.height and 0 <= position[1] < self.width
+
+    def would_loop(self, new_obstacle):
+        # First we will save the current state
+        saved_guard = self.guard
+        saved_visited = {key: val.copy() for (key,val) in self.visited.items()}
+        saved_empty = self.empty.copy()
+        saved_direction = self.direction
+        looping = False
+        # Then we will add the obstacle and check to see if we loop
+        self.obstacles.add(new_obstacle)
+        while(self.is_valid(self.next_position())):
+            # If we have visited the next position already WITH OUR CURRENT
+            # DIRECTION, then we must be looping
+            if self.direction in self.visited.get(self.next_position(), []):
+                looping = True
+                break
+            self.move_guard()
+        # Then we will reset the state to what it was before
+        self.obstacles.remove(new_obstacle)
+        self.guard = saved_guard
+        self.empty = saved_empty
+        self.visited = saved_visited
+        self.direction = saved_direction
+        return looping
     
-    def move_guard(self, wrapping=False):
+    def move_guard(self):
         potential_next = self.next_position()
         if potential_next in self.obstacles:
             # If the guard would hit an obstacle, turn right
             self.turn_right()
+            self.visited[self.guard].append(self.direction)
         else:
             # Otherwise, move the guard and mark the next space as visited
             self.empty.discard(potential_next)
-            self.visited.add(potential_next)
+            if potential_next in self.visited:
+                self.visited[potential_next].append(self.direction)
+            else:
+                self.visited[potential_next] = [self.direction]
             self.guard = potential_next
 
 def part_1(input_str):
@@ -64,10 +92,27 @@ def part_1(input_str):
     return len(eg.visited)
             
 def part_2(input_str):
-    pass
+    # At each step on the guard's journey, check to see if adding an obstacle
+    # right in front of him would cause him to loop
+    #
+    # I'm pretty sure there are way more efficient ways to do this, but this
+    # works, and I am way behind on this year's AoC, so this will do for now
+    eg = Elf_Grid(input_str)
+    looping_obstacles = set()
+    next = eg.next_position()
+    checked_obstacles = set()
+    while(eg.is_valid(next)):
+        # We only need to check for adding an obstacle if:
+        # 1. The space is not already an obstacle, and
+        # 2. We have not already checked that location for an obstacle
+        #    (this prevents miscounting in cases where the path crosses itself)
+        if next not in eg.obstacles and next not in checked_obstacles and eg.would_loop(next):
+            looping_obstacles.add(next)
+        checked_obstacles.add(next)
+        eg.move_guard()
+        next = eg.next_position()
+    return len(looping_obstacles)
             
-    
-
 def main():
     with open('aoc_day_6_input.txt') as input_file:
         full_input = input_file.read()
