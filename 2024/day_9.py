bf7b7c76621738c772e0f3bc869cf936dc842df7
atticuskramer <50434649+atticuskramer@users.noreply.github.com>
2025-01-07 21:10:13 +0000

    Completed 2024 Day 9 Part 2

--- 2024/day_9.py
+++ 2024/day_9.py
@@ -5,7 +5,7 @@ test_input = """2333133121414131402"""
 def part_1(input_str):
     # First, we will build our expanded data list and keep track of the empty spots
     # so that we can later fill them without having to search the list again
-    EMPTY = ','
+    EMPTY = '.'
     is_data = True
     data_val = 0
     expanded = []
@@ -38,7 +38,50 @@ def part_1(input_str):
         
             
 def part_2(input_str):
-    pass
+    # First, we will take our input string, and create a list of empty ranges
+    # and a list of the ranges for each piece of data
+    # The empty ranges are given as (start_idx, length), and the data ranges as
+    # (start_idx, length, value)
+    is_data = True
+    data_val = 0
+    data_ranges = []
+    empty_ranges = []
+    idx = 0
+    for num in [int(num_str) for num_str in input_str]:
+        if is_data:
+            data_ranges.append((idx, num, data_val))
+            data_val += 1
+        else:
+            empty_ranges.append((idx, num))
+        idx += num
+        is_data = not is_data
+    # Next, we move backwards through our data ranges, filling in the empty ranges
+    # if possible, front to back
+    new_data_ranges = []
+    for start_idx, length, value, in data_ranges[::-1]:
+        # Start assuming we will leave the data in place, then update if needed
+        new_range = (start_idx, length, value)
+        for i, (e_start_idx, e_length) in enumerate(empty_ranges):
+            # If our data range starts before the empty range, skip
+            if start_idx < e_start_idx:
+                break
+            if length <= e_length:
+                new_range = (e_start_idx, length, value)
+                # Here we are modifying empty_ranges while looping through it,
+                # which would normally be a problem, but is ok here since we
+                # break immediately afterwards
+                if length == e_length:
+                    empty_ranges = empty_ranges[:i] + empty_ranges[i+1:]
+                else:
+                    empty_ranges[i] = (e_start_idx + length, e_length - length)
+                break
+        new_data_ranges.append(new_range)
+    # Lastly, we go through our new data ranges, and get our checksum
+    checksum = 0
+    for start_idx, length, value in new_data_ranges:
+        for i in range(start_idx, start_idx + length):
+            checksum += i * value
+    return checksum
             
 def main():
     with open('aoc_day_9_input.txt') as input_file:
